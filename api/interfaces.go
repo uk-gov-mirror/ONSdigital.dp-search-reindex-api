bc213472cf1ea82ae88c2a8b0dd64a3e0df79d3d
cookel2 <eleanor.cook@ons.gov.uk>
2021-03-23 12:54:22 +0000

    create storer directory and job_storer class

--- api/interfaces.go
+++ api/interfaces.go
@@ -10,5 +10,5 @@ type JobStorer interface {
 	CreateJob(ctx context.Context, id string, image *models.Image) (err error)
 	GetJobs(ctx context.Context, collectionID string) (images []models.Image, err error)
 	GetJob(ctx context.Context, id string) (image *models.Image, err error)
-	//UpdateJob(ctx context.Context, id string, image *models.Image) (didChange bool, err error)	
+	//UpdateJob(ctx context.Context, id string, image *models.Image) (didChange bool, err error)
 }
