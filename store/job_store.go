4c3b654ca65d26b3d3647e32f12febdcfc64b91e
cookel2 <eleanor.cook@ons.gov.uk>
2021-04-23 16:06:09 +0100

    create a slice and use it to put the jobs in last_updated order

--- store/job_store.go
+++ store/job_store.go
@@ -3,8 +3,11 @@ package store
 import (
 	"context"
 	"errors"
+	"fmt"
 	models "github.com/ONSdigital/dp-search-reindex-api/models"
 	"github.com/ONSdigital/log.go/log"
+	"sort"
+	"time"
 )
 
 type JobStore interface {
@@ -20,6 +23,20 @@ type DataStore struct {
 
 //JobsMap is a map used for storing Job resources with the keys being string values.
 var JobsMap = make(map[string]models.Job)
+
+type timeSlice []models.Job
+
+func (p timeSlice) Len() int {
+	return len(p)
+}
+
+func (p timeSlice) Less(i, j int) bool {
+	return p[i].LastUpdated.Before(p[j].LastUpdated)
+}
+
+func (p timeSlice) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
 
 //DeleteAllJobs empties the JobStore by deleting everything from the JobsMap
 func (ds *DataStore) DeleteAllJobs(ctx context.Context) error {
@@ -88,6 +105,25 @@ func (ds *DataStore) GetJobs(ctx context.Context) (models.Jobs, error) {
 		return jobs, nil
 	}
 
+	//Use a temporary map to order the jobs by last_updated in ascending order (i.e. oldest first)
+	//Start by adding all the jobs from the JobsMap into the tempMap using last_updated as the key
+	//tempMap := make(map[time.Time]models.Job)
+	//ds.MoveJobs(ctx, tempMap)
+
+	//Then delete everything from JobsMap
+	//ds.DeleteAllJobs()
+
+	//Use a sorted slice of last_updated times to put the jobs back into JobsMap but in last_updated order
+	jobsToSort := make(timeSlice, 0, len(JobsMap))
+	for k := range JobsMap {
+		jobsToSort = append(jobsToSort, JobsMap[k])
+	}
+	fmt.Println("before...")
+	fmt.Println(jobsToSort)
+	sort.Sort(jobsToSort)
+	fmt.Println("after...")
+	fmt.Println(jobsToSort)
+
 	//Create an empty list, the same length as the JobsMap, to put the requested Job resources into
 	log.Event(ctx, "generating empty list")
 	jobsList := make([]models.Job, numJobs)
@@ -103,4 +139,13 @@ func (ds *DataStore) GetJobs(ctx context.Context) (models.Jobs, error) {
 	jobs.Job_List = jobsList
 
 	return jobs, nil
+}
+
+//moveJobs moves all the jobs from the JobsMap into the tempMap, which uses last_updated as its key
+func (ds *DataStore) MoveJobs(ctx context.Context, tempMap map[time.Time]models.Job) {
+	for k := range JobsMap {
+		keyVal := JobsMap[k].LastUpdated
+		tempMap[keyVal] = JobsMap[k]
+		log.Event(ctx, "adding job "+k+" to the temporary map", log.Data{"tempMap[" + keyVal.String() + "]": tempMap[keyVal]})
+	}
 }
