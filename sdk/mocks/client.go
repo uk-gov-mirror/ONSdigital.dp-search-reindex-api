49120e09c068803e51a66391ab8abc4df3517d31
cookel2 <eleanor.cook@ons.gov.uk>
2022-03-30 15:43:08 +0100

    regenerate mock client to include PatchJob method

--- sdk/mocks/client.go
+++ sdk/mocks/client.go
@@ -10,35 +10,48 @@ import (
 	"sync"
 )
 
-var (
-	lockClientMockPostJob sync.RWMutex
-)
-
 // Ensure, that ClientMock does implement sdk.Client.
 // If this is not the case, regenerate this file with moq.
 var _ sdk.Client = &ClientMock{}
 
 // ClientMock is a mock implementation of sdk.Client.
 //
-//     func TestSomethingThatUsesClient(t *testing.T) {
+// 	func TestSomethingThatUsesClient(t *testing.T) {
 //
-//         // make and configure a mocked sdk.Client
-//         mockedClient := &ClientMock{
-//             PostJobFunc: func(ctx context.Context, headers sdk.Headers) (models.Job, error) {
-// 	               panic("mock out the PostJob method")
-//             },
-//         }
+// 		// make and configure a mocked sdk.Client
+// 		mockedClient := &ClientMock{
+// 			PatchJobFunc: func(ctx context.Context, headers sdk.Headers, jobID string, body sdk.PatchOpsList) error {
+// 				panic("mock out the PatchJob method")
+// 			},
+// 			PostJobFunc: func(ctx context.Context, headers sdk.Headers) (models.Job, error) {
+// 				panic("mock out the PostJob method")
+// 			},
+// 		}
 //
-//         // use mockedClient in code that requires sdk.Client
-//         // and then make assertions.
+// 		// use mockedClient in code that requires sdk.Client
+// 		// and then make assertions.
 //
-//     }
+// 	}
 type ClientMock struct {
+	// PatchJobFunc mocks the PatchJob method.
+	PatchJobFunc func(ctx context.Context, headers sdk.Headers, jobID string, body sdk.PatchOpsList) error
+
 	// PostJobFunc mocks the PostJob method.
 	PostJobFunc func(ctx context.Context, headers sdk.Headers) (models.Job, error)
 
 	// calls tracks calls to the methods.
 	calls struct {
+		// PatchJob holds details about calls to the PatchJob method.
+		PatchJob []struct {
+			// Ctx is the ctx argument value.
+			Ctx context.Context
+			// Headers is the headers argument value.
+			Headers sdk.Headers
+			// JobID is the jobID argument value.
+			JobID string
+			// Body is the body argument value.
+			Body sdk.PatchOpsList
+		}
 		// PostJob holds details about calls to the PostJob method.
 		PostJob []struct {
 			// Ctx is the ctx argument value.
@@ -47,6 +60,51 @@ type ClientMock struct {
 			Headers sdk.Headers
 		}
 	}
+	lockPatchJob sync.RWMutex
+	lockPostJob  sync.RWMutex
+}
+
+// PatchJob calls PatchJobFunc.
+func (mock *ClientMock) PatchJob(ctx context.Context, headers sdk.Headers, jobID string, body sdk.PatchOpsList) error {
+	if mock.PatchJobFunc == nil {
+		panic("ClientMock.PatchJobFunc: method is nil but Client.PatchJob was just called")
+	}
+	callInfo := struct {
+		Ctx     context.Context
+		Headers sdk.Headers
+		JobID   string
+		Body    sdk.PatchOpsList
+	}{
+		Ctx:     ctx,
+		Headers: headers,
+		JobID:   jobID,
+		Body:    body,
+	}
+	mock.lockPatchJob.Lock()
+	mock.calls.PatchJob = append(mock.calls.PatchJob, callInfo)
+	mock.lockPatchJob.Unlock()
+	return mock.PatchJobFunc(ctx, headers, jobID, body)
+}
+
+// PatchJobCalls gets all the calls that were made to PatchJob.
+// Check the length with:
+//     len(mockedClient.PatchJobCalls())
+func (mock *ClientMock) PatchJobCalls() []struct {
+	Ctx     context.Context
+	Headers sdk.Headers
+	JobID   string
+	Body    sdk.PatchOpsList
+} {
+	var calls []struct {
+		Ctx     context.Context
+		Headers sdk.Headers
+		JobID   string
+		Body    sdk.PatchOpsList
+	}
+	mock.lockPatchJob.RLock()
+	calls = mock.calls.PatchJob
+	mock.lockPatchJob.RUnlock()
+	return calls
 }
 
 // PostJob calls PostJobFunc.
@@ -61,9 +119,9 @@ func (mock *ClientMock) PostJob(ctx context.Context, headers sdk.Headers) (model
 		Ctx:     ctx,
 		Headers: headers,
 	}
-	lockClientMockPostJob.Lock()
+	mock.lockPostJob.Lock()
 	mock.calls.PostJob = append(mock.calls.PostJob, callInfo)
-	lockClientMockPostJob.Unlock()
+	mock.lockPostJob.Unlock()
 	return mock.PostJobFunc(ctx, headers)
 }
 
@@ -78,8 +136,8 @@ func (mock *ClientMock) PostJobCalls() []struct {
 		Ctx     context.Context
 		Headers sdk.Headers
 	}
-	lockClientMockPostJob.RLock()
+	mock.lockPostJob.RLock()
 	calls = mock.calls.PostJob
-	lockClientMockPostJob.RUnlock()
+	mock.lockPostJob.RUnlock()
 	return calls
 }
