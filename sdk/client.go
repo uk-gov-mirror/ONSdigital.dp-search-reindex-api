5c2ec5da0d3d9c48cb73a7c8537754375316a93a
cookel2 <eleanor.cook@ons.gov.uk>
2022-05-10 09:06:14 +0100

    go fmt

--- sdk/client.go
+++ sdk/client.go
@@ -23,7 +23,7 @@ type Client interface {
 	GetTask(ctx context.Context, headers Headers, jobID, taskName string) (*RespHeaders, *models.Task, error)
 	GetJob(ctx context.Context, reqheader Headers, jobID string) (*RespHeaders, *models.Job, error)
 	GetTasks(ctx context.Context, reqheader Headers, jobID string) (*RespHeaders, *models.Tasks, error)
-    PutJobNumberOfTasks(ctx context.Context, reqHeaders Headers, jobID, numTasks string) (*RespHeaders, error)
+	PutJobNumberOfTasks(ctx context.Context, reqHeaders Headers, jobID, numTasks string) (*RespHeaders, error)
 	URL() string
 }
 
