538c5431a6e9a7e05c8a48d3154c52026dba7e2f
cookel2 <eleanor.cook@ons.gov.uk>
2022-07-07 17:58:31 +0100

    replace check for mgo.ErrNotFound with check for mongodriver.ErrNoDocumentFound

--- mongo/jobs.go
+++ mongo/jobs.go
@@ -3,13 +3,12 @@ package mongo
 import (
 	"context"
 	"errors"
-	mongodriver "github.com/ONSdigital/dp-mongodb/v3/mongodb"
-	"github.com/ONSdigital/dp-search-reindex-api/config"
 	"time"
 
+	mongodriver "github.com/ONSdigital/dp-mongodb/v3/mongodb"
+	"github.com/ONSdigital/dp-search-reindex-api/config"
 	"github.com/ONSdigital/dp-search-reindex-api/models"
 	"github.com/ONSdigital/log.go/v2/log"
-	"github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
 )
 
@@ -106,7 +105,7 @@ func (m *JobStore) GetJob(ctx context.Context, id string) (*models.Job, error) {
 
 	job, err := m.findJob(ctx, id)
 	if err != nil {
-		if err == mgo.ErrNotFound {
+		if errors.Is(err, mongodriver.ErrNoDocumentFound) {
 			log.Error(ctx, "job not found in mongo", err, logData)
 			return nil, ErrJobNotFound
 		}
